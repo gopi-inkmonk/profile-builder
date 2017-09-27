e4e8b29f5d4c22b2d1d85ec4d1c5f8facf32cf05
Gopi Raja <gopi@inkmonk.com>
2017-09-27 18:17:40 +0530

    home page HTMLized

--- profile/src/components/LandingPage.js
+++ profile/src/components/LandingPage.js
@@ -20,37 +20,121 @@ class LandingPage extends Component {
             property="og:description"
             content="Your professional Bio can speak lot more than your CV. Build your online Bio for free."
           />
+
+          <link
+            href="https://fonts.googleapis.com/css?family=Volkhov:400,700"
+            rel="stylesheet"
+          />
           {/* <meta property="og:image" content={this.props.DPImage} /> */}
         </Helmet>
 
-        <div className="banner">
-          <div>
-            <a href="/">Itsmybio.me</a>
-            <div>
-              <a href="/">Sign In</a>
-              <a href="/">
-                Sign Up <FontAwesome name="long-arrow-right" />
+        <div className="pageWrapper">
+          <div className="banner">
+            <div className="container">
+              <div className="topStrip">
+                <a className="brand" href="/">
+                  Itsmybio.me
+                </a>
+                <div className="signCTA">
+                  <a href="https://account.itsmybio.me">Sign In</a>
+                  <a href="https://account.itsmybio.me/register">
+                    Sign Up <FontAwesome name="long-arrow-right" />
+                  </a>
+                </div>
+              </div>
+
+              <h3>How do we know</h3>
+              <h1>Morarji Desai lived 99 years?</h1>
+
+              <p>
+                It is written in their Biography.<br />
+                So, Why don't you write your own bio now?
+              </p>
+
+              <p>
+                Your professional Bio can speak lot more than your CV.<br />
+                Build your online <span>Bio</span> for free.
+              </p>
+
+              <a
+                href="https://account.itsmybio.me/register"
+                className="btn signup"
+              >
+                <span>Write your Bio for free</span>{' '}
+                <FontAwesome name="long-arrow-right" />
               </a>
+              <img src={require('../images/macbook.png')} width="495" />
             </div>
           </div>
 
-          <h3>How do we know</h3>
-          <h1>Morarji Desai lived 99 years?</h1>
+          <div className="homeContWrapper">
+            <h5>More about itsmybio.me</h5>
 
-          <p>
-            It is written in their Biography.<br />
-            So, Why don't you write your own bio now?
-          </p>
+            <div className="secRow imgRight">
+              <div>
+                <img src={require('../images/001.png')} width="395" />
+              </div>
+              <div>
+                <p>Get your unique URL</p>
+              </div>
+            </div>
 
-          <p>
-            Your professional Bio can speak lot more than your CV.<br />
-            Build your online <span>Bio</span> for free.
-          </p>
+            <div className="secRow">
+              <div>
+                <img src={require('../images/002.png')} width="300" />
+              </div>
+              <div>
+                <p>Get your professional online identity</p>
+              </div>
+            </div>
 
-          <a href="/">
-            Write your Bio for free <FontAwesome name="long-arrow-right" />
-          </a>
-          <img src={require('../images/macbook.png')} width="495" />
+            <div className="secRow imgRight">
+              <div>
+                <img src={require('../images/003.png')} width="300" />
+              </div>
+              <div>
+                <p>Present who you are and what you do in one URL</p>
+              </div>
+            </div>
+
+            <div className="secRow">
+              <div>
+                <img src={require('../images/004.png')} width="300" />
+              </div>
+              <div>
+                <p>Bio will speak louder than CV</p>
+              </div>
+            </div>
+
+            <div className="secRow imgRight">
+              <div>
+                <img src={require('../images/005.png')} width="300" />
+              </div>
+              <div>
+                <p>
+                  Write your Bio. So, that people will know what you really are
+                </p>
+              </div>
+            </div>
+
+            <div className="secRow">
+              <div>
+                <img src={require('../images/006.png')} width="300" />
+              </div>
+              <div>
+                <p>Your profile on itsmybio.me is Mobile friendly</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer">
+            <span>
+              Made with <FontAwesome name="heart" /> in india
+            </span>
+            <span>
+              <FontAwesome name="copyright" /> All rights reserved
+            </span>
+          </div>
         </div>
       </div>
     );
