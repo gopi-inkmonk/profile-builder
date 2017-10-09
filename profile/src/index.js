a7b9ace464ac37da9ae07634dba5227154890331
Gopi Raja <gopi@inkmonk.com>
2017-10-09 12:13:30 +0530

    profile moved to seperate repo

--- profile/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './stylesheets/profile.css';
-import AppRoutes from './AppRoutes';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
-registerServiceWorker();
